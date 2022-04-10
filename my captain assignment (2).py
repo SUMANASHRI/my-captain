f88a8be60fa50facf9d26e270200507e4ea34fbb
SUMANASHRI <101055727+SUMANASHRI@users.noreply.github.com>
2022-04-10 15:49:02 +0530

    Add files via upload

---
+++ my captain assignment (2).py
@@ -0,0 +1,14 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+numone = 12
+numtwo = 12
+numone+numtwo
+24
+numone = 10
+numtwo = 6
+numone-numtwo
+4
+numone = 2
+numtwo = 2
+numone*numtwo
+4
